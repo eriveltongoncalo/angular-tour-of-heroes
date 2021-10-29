c1d91a3021ca0689eb390cf9f224eb59a90059fd
Erivelton Jesus <eriveltongoncalo@gmail.com>
2021-10-29 18:04:19 -0400

    Implementation of MessageComponent and MessageService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+
   ],
   imports: [
     BrowserModule,
