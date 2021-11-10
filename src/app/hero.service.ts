83223536bdb3e8416f99e9118668b3b46201472b
Erivelton Jesus <erivelton.jesus@tre-mt.jus.br>
2021-11-10 17:42:21 -0400

    Refactory getHero method.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,7 +12,7 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  private heroesUrl = 'api/heroes'; // URL to web api
+  private heroesUrl = 'api/heroe,s'; // URL to web api
 
   constructor(
     private http: HttpClient,
@@ -22,6 +22,7 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
+        tap(_ => this.log('fetched heroes')),
         catchError(this.handleError<Hero[]>('getHeroes',[]))
       );
   }
@@ -48,14 +49,14 @@ export class HeroService {
     };
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
 
-    // For now, assume that a hero with the specified `id` always exists.
-    // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-
-    return of(hero) ;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /*Log a HeroService message with the MessageService */
